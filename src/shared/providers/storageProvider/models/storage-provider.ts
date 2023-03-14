302c0579b291d2b2065e3f4ec57af376d957a5e4
Tiaguin061 <tiaguin180@gmail.com>
2023-03-14 14:43:30 -0300

    feat: Created  updateFile method on storageProvider

--- src/shared/providers/storageProvider/models/storage-provider.ts
+++ src/shared/providers/storageProvider/models/storage-provider.ts
@@ -3,7 +3,14 @@ export interface SaveFileInput {
   buffer: Buffer;
 }
 
+export interface UpdateFileInput {
+  newFilename: string;
+  oldFilename: string;
+  newFilenameBuffer: Buffer;
+}
+
 export abstract class StorageProvider {
   abstract saveFile(data: SaveFileInput): Promise<string>;
   abstract deleteFile(filename: string): Promise<void>;
+  abstract updateFile(data: UpdateFileInput): Promise<string>;
 }
