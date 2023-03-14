7262cf5f0e34758b634660ee5aa94dfc581f8abe
Tiaguin061 <tiaguin180@gmail.com>
2023-03-14 11:23:04 -0300

    refactor: Mock a values in memory storage provider

--- src/modules/restaurant/__tests__/inMemory/storage-provider.ts
+++ src/modules/restaurant/__tests__/inMemory/storage-provider.ts
@@ -1,32 +1,18 @@
-import { SaveFileInput, StorageProvider } from 'src/shared/providers/storageProvider/models/storage-provider';
+import {
+  SaveFileInput,
+  StorageProvider,
+} from 'src/shared/providers/storageProvider/models/storage-provider';
 
 import crypto from 'crypto';
-import fs from 'fs';
-import path from 'path';
-import { storageConfig } from 'src/config/storage';
 
-export class inMemoryStorageProvider implements StorageProvider {
-  async saveFile({ buffer, filename }: SaveFileInput): Promise<string> {
+export class InMemoryStorageProvider implements StorageProvider {
+  async saveFile({ filename }: SaveFileInput): Promise<string> {
     const id = crypto.randomUUID();
 
-    await fs.promises.writeFile(`./tmp/uploads/${id}-${filename}`, buffer);
-    
     return `${id}-${filename}`;
   }
 
   async deleteFile(filename: string): Promise<void> {
-    const filePath = path.resolve(
-      storageConfig.config.disk.paths.uploadsFolder,
-      filename,
-    );
-
-    try {
-      await fs.promises.stat(filePath);
-    } catch {
-      return;
-    }
-
-    await fs.promises.unlink(filePath);
+    return;
   }
 }
-
