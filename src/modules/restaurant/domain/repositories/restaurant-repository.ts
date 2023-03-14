8b0fdff850e217f77b646070a419969475f3d746
Tiaguin061 <tiaguin180@gmail.com>
2023-03-14 11:24:50 -0300

    feat: Added new methods in memory restaurant repository

--- src/modules/restaurant/domain/repositories/restaurant-repository.ts
+++ src/modules/restaurant/domain/repositories/restaurant-repository.ts
@@ -2,4 +2,9 @@ import { IRestaurant } from '../entities/restaurant';
 
 export abstract class RestaurantRepository {
   public abstract create(restaurant: IRestaurant): Promise<IRestaurant>;
+  public abstract listMany(): Promise<IRestaurant[]>;
+
+  public abstract findUniqueById(
+    restaurant_id: string,
+  ): Promise<IRestaurant | null>;
 }
