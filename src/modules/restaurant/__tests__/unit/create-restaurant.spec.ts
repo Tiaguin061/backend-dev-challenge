15186c4d3d6dd47908f9cccf481f0d432700fd42
Tiaguin061 <tiaguin180@gmail.com>
2023-03-14 11:24:28 -0300

    feat: Created test to verify if photo is being save on create restaurant

--- src/modules/restaurant/__tests__/unit/create-restaurant.spec.ts
+++ src/modules/restaurant/__tests__/unit/create-restaurant.spec.ts
@@ -1,34 +1,59 @@
 import { describe, expect, it } from '@jest/globals';
 
+import { AbstractCreateRestaurantService } from '../../domain/services/restaurant-services';
 import { CreateRestaurantService } from '../../services/create-restaurant.service';
 import { InMemoryRestaurantRepository } from '../inMemory/restaurant-repository';
+import { InMemoryStorageProvider } from '../inMemory/storage-provider';
 import { Restaurant } from '../../domain/entities/restaurant';
 import { RestaurantRepository } from '../../domain/repositories/restaurant-repository';
 import { StorageProvider } from 'src/shared/providers/storageProvider/models/storage-provider';
 
 let inMemoryRestaurantRepository: RestaurantRepository;
 let inMemoryStorageProvider: StorageProvider;
-let createRestaurant: CreateRestaurantService;
+let createRestaurantService: AbstractCreateRestaurantService;
 
 describe('create-restaurant', () => {
   beforeEach(() => {
     inMemoryRestaurantRepository = new InMemoryRestaurantRepository();
-    createRestaurant = new CreateRestaurantService(
+    inMemoryStorageProvider = new InMemoryStorageProvider();
+    createRestaurantService = new CreateRestaurantService(
       inMemoryRestaurantRepository,
       inMemoryStorageProvider,
     );
   });
 
-  it('should be able create a new restaurant', async () => {
+  it('should be able to create a new restaurant without profile_photo', async () => {
+    const fakeRestaurant = new Restaurant({
+      name: 'fake-name',
+      address: 'fake-address',
+      opening_hour: 'fake-opening_hour',
+      user_id: 'fake-user_id',
+    }).restaurant;
+
+    const restaurant = await createRestaurantService.execute(fakeRestaurant);
+
+    expect(restaurant.id);
+  });
+
+  it('should be able create a new restaurant with phofile_photo', async () => {
     const fakeRestaurant = new Restaurant({
       name: 'fake-name',
       address: 'fake-address',
       opening_hour: 'fake-opening_hour',
       profile_photo: 'fake-profile_photo.png',
       user_id: 'fake-user_id',
-    });
+    }).restaurant;
 
-    const restaurant = await createRestaurant.execute(fakeRestaurant);
+    if (fakeRestaurant.profile_photo) {
+      let buffer: Buffer;
+
+      fakeRestaurant.profile_photo = await inMemoryStorageProvider.saveFile({
+        buffer,
+        filename: fakeRestaurant.profile_photo,
+      });
+    }
+
+    const restaurant = await createRestaurantService.execute(fakeRestaurant);
 
     expect(restaurant.id);
   });
