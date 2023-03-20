752ee48a2f79ea6517b2187831b4627af97cb4d9
Tiaguin061 <tiaguin180@gmail.com>
2023-03-20 00:34:29 -0300

    feat: Created infra database layer on restaurant-product-promotion module

---
+++ src/modules/restaurant-product-promotion/infra/database/prisma/repositories/restaurant-product-promotion-repository.ts
@@ -0,0 +1,60 @@
+import {
+  CreateRestaurantProductPromotionRepositoryData,
+  UpdateUniqueRestaurantProductPromotionRepositoryData,
+} from '@root/modules/restaurant-product-promotion/domain/repositories/types';
+
+import { Injectable } from '@nestjs/common';
+import { RestaurantProductPromotionRepository } from '@root/modules/restaurant-product-promotion/domain/repositories/restaurant-product-promotion-repository';
+import { prisma } from '@root/shared/infra/database/prisma/client';
+
+@Injectable()
+export class PrismaRestaurantProductPromotionRepository
+  implements RestaurantProductPromotionRepository
+{
+  async create({
+    description,
+    promotional_price,
+    restaurant_product_id,
+    start_promotion_date,
+    end_promotion_date,
+  }: CreateRestaurantProductPromotionRepositoryData) {
+    return prisma.restaurantProductPromocion.create({
+      data: {
+        description,
+        promotional_price,
+        restaurant_product_id,
+        start_promotion_date,
+        end_promotion_date,
+      },
+      include: {
+        restaurantProduct: true,
+      },
+    });
+  }
+
+  async updateUnique({
+    data,
+    restaurant_product_promotion_id,
+  }: UpdateUniqueRestaurantProductPromotionRepositoryData) {
+    return prisma.restaurantProductPromocion.update({
+      where: {
+        id: restaurant_product_promotion_id,
+      },
+      data,
+      include: {
+        restaurantProduct: true,
+      },
+    });
+  }
+
+  async findUniqueById(restaurant_product_promocion: string) {
+    return prisma.restaurantProductPromocion.findUnique({
+      where: {
+        id: restaurant_product_promocion,
+      },
+      include: {
+        restaurantProduct: true,
+      },
+    });
+  }
+}
