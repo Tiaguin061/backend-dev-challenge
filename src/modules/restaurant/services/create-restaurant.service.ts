50d406114f44718354bf3e98c91ca41c8cfa4d30
Tiaguin061 <tiaguin180@gmail.com>
2023-03-24 14:16:09 -0300

    feat: Verify if user exist to create restaurant on service

--- src/modules/restaurant/services/create-restaurant.service.ts
+++ src/modules/restaurant/services/create-restaurant.service.ts
@@ -1,8 +1,10 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+
 import { AbstractCreateRestaurantService } from '@root/modules/restaurant/domain/services/restaurant-services';
 import { CreateRestaurantServiceData } from '@root/modules/restaurant/domain/services/types';
-import { Injectable } from '@nestjs/common';
 import { RestaurantRepository } from '@root/modules/restaurant/domain/repositories/restaurant-repository';
 import { StorageProvider } from '@root/shared/providers/storageProvider/models/storage-provider';
+import { UserRepository } from '@root/modules/user/domain/repositories/user-respository';
 
 @Injectable()
 export class CreateRestaurantService
@@ -11,9 +13,16 @@ export class CreateRestaurantService
   constructor(
     private restaurantRepository: RestaurantRepository,
     private storageProvider: StorageProvider,
+    private userRepository: UserRepository,
   ) {}
   async execute(data: CreateRestaurantServiceData) {
     const { profile_photo_file, ..._data } = data;
+
+    const foundUser = await this.userRepository.findUniqueById(_data.user_id);
+
+    if (!foundUser) {
+      throw new BadRequestException('This user does not exist.');
+    }
 
     let profile_photo = null;
 
