d0ab7d36544074e6116223c6d9460da6ab8d7e82
Tiaguin061 <tiaguin180@gmail.com>
2023-03-26 14:16:06 -0300

    feat: Added auto generation docs using treblle

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { useNestTreblle } from 'treblle';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const expressInstance = app.getHttpAdapter().getInstance();
+
+  if (process.env.NODE_ENV !== 'production') {
+    // Generate automatically docs on treblle application
+    useNestTreblle(expressInstance, {
+      apiKey: 'gRLqFMxQ4anu3ANK1kwm6Md3rzgmpuZR',
+      projectId: 'Aq1Ahdu8sfphUzUK',
+    });
+  }
+
   await app.listen(3333);
 }
 bootstrap();
