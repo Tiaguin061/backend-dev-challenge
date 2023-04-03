923e435ff7e3a727fa4a8eda8d2afb1d1ee6b11d
Tiaguin061 <tiaguin180@gmail.com>
2023-04-03 17:21:54 -0300

    refactor: Added cors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,34 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { useNestTreblle } from 'treblle';
+import cors from 'cors';
+
+function swaggerSetup(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Goomer rango Restaurants')
+    .setDescription('All api routers from aplication')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const expressInstance = app.getHttpAdapter().getInstance();
+  app.use(cors({ origin: '*' }));
 
-  if (process.env.NODE_ENV !== 'production') {
-    // Generate automatically docs on treblle application
-    useNestTreblle(expressInstance, {
-      apiKey: 'gRLqFMxQ4anu3ANK1kwm6Md3rzgmpuZR',
-      projectId: 'Aq1Ahdu8sfphUzUK',
-    });
-  }
+  const appPort = process.env.PORT || 3333;
 
-  await app.listen(3333);
+  swaggerSetup(app);
+
+  await app.listen(appPort);
+
+  console.log(`App listening on port ${appPort}`);
 }
+
 bootstrap();
